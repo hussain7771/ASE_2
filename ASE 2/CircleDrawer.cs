aeeab71271996bac9a1dd3aedabccf89939b9f52
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2024-01-23 15:26:46 -0800

    Updated CircleDrawer

--- ASE 2/CircleDrawer.cs
+++ ASE 2/CircleDrawer.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASE_2
 {
-    internal class CircleDrawer
+    public class CircleDrawer
     {
+        private Graphics g;
+        private Pen pen;
+        private Brush brush;
+
+
+        public CircleDrawer(Graphics graphics, Pen pen, Brush brush)
+        {
+            this.g = graphics;
+            this.pen = pen;
+            this.brush = brush;
+
+        }
+
+        public void DrawCircle(int circleRadius, int x, int y)
+        {
+
+            Rectangle circleBounds = new Rectangle(x - circleRadius, y - circleRadius, 2 * circleRadius, 2 * circleRadius);
+
+            g.DrawEllipse(pen, circleBounds);
+
+
+        }
     }
 }
