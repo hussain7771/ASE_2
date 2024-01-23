d9382aab94ee5b85c5c33457f56800dd367acaf0
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2024-01-23 15:27:35 -0800

    Updated Rectangel Drawer file

--- ASE 2/RectangleDrawer.cs
+++ ASE 2/RectangleDrawer.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASE_2
 {
-    internal class RectangleDrawer
+    public class RectangleDrawer
     {
+        private Graphics g;
+        private Pen pen;
+        private Brush brush;
+
+        public RectangleDrawer(Graphics graphics, Pen pen, Brush brush)
+        {
+            this.g = graphics;
+            this.pen = pen;
+            this.brush = brush;
+        }
+
+        public void DrawRectangle(int width, int height, int x, int y)
+        {
+            g.DrawRectangle(pen, x, y, width, height);
+        }
     }
 }
