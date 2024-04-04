604935774aa0a5263578997ee691c4e546c0d6e9
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2024-04-03 19:10:10 -0700

    XML Comments for square drawer

--- ASE 2/SquareDrawer.cs
+++ ASE 2/SquareDrawer.cs
@@ -1,31 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
 
 namespace ASE_2
 {
+    /// <summary>
+    /// Represents a utility class for drawing squares on a graphics surface.
+    /// </summary>
     public class SquareDrawer
     {
         private Graphics g;
         private Pen pen;
 
+        /// <summary>
+        /// Initializes a new instance of the SquareDrawer class with the specified graphics and pen.
+        /// </summary>
+        /// <param name="graphics">The Graphics object to draw on.</param>
+        /// <param name="pen">The Pen object to use for drawing the square's outline.</param>
         public SquareDrawer(Graphics graphics, Pen pen)
         {
             this.g = graphics;
             this.pen = pen;
         }
 
-        public static void DrawSquare()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Draws a square with the specified size and position.
+        /// </summary>
+        /// <param name="size">The size of the square (width and height).</param>
+        /// <param name="x">The x-coordinate of the top-left corner of the square.</param>
+        /// <param name="y">The y-coordinate of the top-left corner of the square.</param>
         public void DrawSquare(int size, int x, int y)
-        {  
-            // Draw a square using the specified size and location sfsw 
+        {
+            // Draw a square using the specified size and location
             g.DrawRectangle(pen, x, y, size, size);
         }
     }
