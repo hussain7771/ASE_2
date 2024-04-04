9515f95a565f6d98a029df687a4bd69a52809750
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2024-04-03 17:32:49 -0700

    CircleDrawerTest

---
+++ ASE Test/CircleDrawerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASE_2;
+
+namespace ASE_2_Test
+{
+    [TestClass]
+    public class CircleDrawerTests
+    {
+        [TestMethod]
+        public void DrawCircle_PositionLessThanCondition()
+        {
+            // Arrange
+            Bitmap bitmap = new Bitmap(800, 600);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(Color.Black);
+            Brush brush = Brushes.Red;
+            CircleDrawer circleDrawer = new CircleDrawer(graphics, pen, brush);
+            int x = 5; // x-coordinate less than 10
+            int y = 100;
+            int radius = 50;
+
+            // Act
+            circleDrawer.DrawCircle(radius, x, y);
+
+            // Assert
+            // Check if the pixel at the center of the circle is affected
+            Color centerPixelColor = bitmap.GetPixel(x, y);
+            Assert.AreNotEqual(Color.Transparent, centerPixelColor); // Assuming the circle is filled and not transparent
+        }
+    }
+}
